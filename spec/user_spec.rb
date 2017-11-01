3762d6a763a33ebbca669394b3a88dc6dff7bd86
BeejLuig <bjcantlupe@gmail.com>
2017-11-01 15:48:35 -0400

    initial commit

---
+++ spec/user_spec.rb
@@ -0,0 +1,36 @@
+require 'faker'
+require_relative '../user.rb'
+
+RSpec.describe User do
+  describe '.new' do
+    it 'creates a new User instance' do
+      user = build(:user)
+      puts attributes_for(:user)
+      expect(user).to be_a(User)
+    end
+  end
+
+  describe 'attribute methods' do
+
+    user = nil
+    before(:each) { user = build(:user) }
+
+    it "#name returns the User's name" do
+      expect { user.name }.not_to raise_error
+      expect(user.name).to be_a(String)
+    end
+
+    it "#name= resets the User's name" do
+      expect { user.name = 'Joey' }.not_to raise_error
+      expect(user.name).to eq('Joey')
+    end
+  end
+
+end
+
+
+
+
+
+
+#
